4e6be748e24ae85623ced6d81960eb1c6f3b7ce6
Jesus Palacio Yancy <jpalacio@transportationamerica.com>
2024-10-15 17:44:28 -0500

    Inicio seccion 11

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrl: './basics-page.component.css'
+})
+export class BasicsPageComponent {
+
+  public nameLower: string = 'JESUS PALACIO';
+  public nameUpper: string = 'jesus palacio';
+  public nameTitle: string = 'jEsUs pAlAcIo'
+}
